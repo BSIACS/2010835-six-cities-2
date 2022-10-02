f981403bb22637ec612020fe8ab95fe806524f5f
BSI <bsi_acs@mail.ru>
2022-10-03 02:36:29 +0300

    Добавлен сервис для установки соединения с БД

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ import { LoggerInterface } from './logger/logger.interface.js';
 import LoggerService from './logger/logger.service.js';
 import { Component } from './types/component.types.js';
 import 'reflect-metadata';
+import { DatabaseInterface } from './common/database-client/database.interface.js';
+import DatabaseService from './common/database-client/database.service.js';
 
 const applicationContainer = new Container();
 applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
 applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
+applicationContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
